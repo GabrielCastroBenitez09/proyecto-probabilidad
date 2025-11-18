6ac8b23e003457bba7ba759dc23d1bbb4ddadc19
GabrielCastroBenitez09 <gabcastrobenitez009@gmail.com>
2025-11-17 20:19:35 -0500

    Modificaciones

--- scripts/analisis_exploratorio.R
+++ scripts/analisis_exploratorio.R
@@ -4,72 +4,61 @@ install.packages("viridis")
 
 
 # Librerias
+library(tidyr)
 library(dplyr)
 library(ggplot2)
 library(ggthemes)
 library(viridis)
+library(forcats)
 
 
 # Base de Datos
 homicidios <- readRDS("datos/Homicidios_Limpios_2015_2024.rds")
 names(homicidios)
+table(homicidios$escenario_del_hecho)
 
+# Análisis
 # Año del Hecho
-ggplot(homicidios, aes(x = factor(ano_del_hecho), fill = factor(ano_del_hecho))) + 
-    geom_bar(width = 0.7) + 
-    theme_light() +
-    scale_fill_viridis(discrete = TRUE, option = "D") + # Viridis escala automáticamente a 10 colores
-    labs(x = "Año del Hecho", y = "Número de Homicidios") +
-    theme(legend.position = "none") +
-    scale_x_discrete(expand = expansion(add = 0.5)) +
-    ggtitle("Homicidios por Año (2015 - 2024)")
+table(homicidios$ano_del_hecho)
+
+
 
 # Mes del Hecho
-homicidios %>%
-    drop_na(dia_del_hecho) %>%
-    ggplot(aes(x = mes_del_hecho, fill = mes_del_hecho)) + 
-        geom_bar(width = 0.7) + 
-        theme_light() +
-        scale_fill_viridis(discrete = TRUE, option = "D") + # Viridis escala automáticamente a 10 colores
-        labs(x = "Mes del Hecho", y = "Número de Homicidios") +
-        theme(legend.position = "none") +
-        scale_x_discrete(expand = expansion(add = 0.5)) +
-        ggtitle("Homicidios por Mes")
+table(homicidios$mes_del_hecho)
+
+
 
 # Día del Hecho
-homicidios %>%
-    drop_na(dia_del_hecho) %>%
-    ggplot(aes(x = dia_del_hecho, fill = dia_del_hecho)) + 
-        geom_bar(width = 0.7) + 
-        theme_light() +s
-        scale_fill_viridis(discrete = TRUE, option = "D") + # Viridis escala automáticamente a 10 colores
-        labs(x = "Día de la Semana", y = "Número de Homicidios") +
-        theme(legend.position = "none") +
-        scale_x_discrete(expand = expansion(add = 0.5)) +
-        ggtitle("Homicidios por Día de la Semana")
+table(homicidios$dia_del_hecho)
+
+
 
 # Mecanismo Causal
-ggplot(homicidios, aes(x = mecanismo_causal_de_la_lesion_fatal, fill = mecanismo_causal_de_la_lesion_fatal)) + 
-    geom_bar(width = 0.7) + 
-    theme_light() +
-    coord_flip() +
-    scale_fill_viridis(discrete = TRUE, option = "D") + # Viridis escala automáticamente a 10 colores
-    labs(x = "Mecanismo Causal", y = "Número de Homicidios") +
-    theme(legend.position = "none") +
-    scale_x_discrete(expand = expansion(add = 0.5)) +
-    ggtitle("Homicidios por Mecanismo Causal")
+
+
+# Grupo de Edad
+table(homicidios$grupo_de_edad_quinquenal)
+
+
+
+# Grupo Mayor/Menor de Edad
+
+
+# Localidad (Bogotá)
+
+
+# Departamento 
+table(homicidios$departamento_del_hecho_dane)
 
 
 
 
+# Sexo
+table(homicidios$sexo_de_la_victima)
 
 
 
-
-
-
-
-
+# Actividad Durante el Hecho
 
 
 
--- scripts/limpieza_datos.R
+++ scripts/limpieza_datos.R
@@ -6,6 +6,7 @@ install.packages("janitor")
 library(tidyverse)
 library(dplyr)
 library(janitor)
+library(stringr)
 
 
 # Base de Datos
@@ -54,13 +55,77 @@ columnas_character <- Homicidio_2015_2024 %>%
 names(columnas_character)
 
 columnas_character <- columnas_character %>%
-    select(-municipio_del_hecho_dane, -departamento_del_hecho_dane, -localidad_del_hecho)
+    select(-municipio_del_hecho_dane)
 names(columnas_character)
 
 for (columna in names(columnas_character)){
     print(paste("---", columna, "---"))
     print(unique(Homicidio_2015_2024[[columna]]))
     }
+table(Homicidio_2015_2024$presunto_agresor_detallado)
+
+Homicidio_2015_2024 <- Homicidio_2015_2024 %>%
+    mutate(
+        escenario_del_hecho = as.character(escenario_del_hecho), 
+        escenario_del_hecho = tolower(escenario_del_hecho),
+        escenario_del_hecho = gsub("\\s*\\(.*\\)", "", escenario_del_hecho),
+        escenario_del_hecho = str_trim(escenario_del_hecho),
+
+        actividad_durante_el_hecho = case_when(
+            actividad_durante_el_hecho == "Actividades de desplazamiento de un lugar a otro." ~ 
+            "Actividades de desplazamiento de un lugar a otro",
+
+            actividad_durante_el_hecho == "Actividades relacionadas con los deportes y el ejercicio físico." ~ 
+            "Actividades relacionadas con los deportes y el ejercicio físico",
+
+            actividad_durante_el_hecho == "Actividades relacionadas con manifestaciones públicas (Marchas, protestas, etc)" ~ 
+            "Actividades relacionadas con manifestaciones públicas (marchas, protestas, etc)",
+
+            actividad_durante_el_hecho == "Actividades relacionadas con manifestaciones públicas (Marchas, protestas, etc.)" ~ 
+            "Actividades relacionadas con manifestaciones públicas (marchas, protestas, etc)",
+
+            actividad_durante_el_hecho == "" ~ "",
+            actividad_durante_el_hecho == "" ~ "",
+            TRUE ~ actividad_durante_el_hecho
+        ),
+
+        presunto_agresor_detallado = case_when(
+            presunto_agresor_detallado == "Abuelo (a)" ~ "Abuelo(a)",
+            presunto_agresor_detallado == "Amigo" ~ "Amigo(a)",
+            presunto_agresor_detallado == "Amigo (a)" ~ "Amigo(a)",
+            presunto_agresor_detallado == "Barra(s) futboleta(s)" ~ "Barra(s) futbolera(s)",
+            presunto_agresor_detallado == "Barra(s) futboleras" ~ "Barra(s) futbolera(s)",
+            presunto_agresor_detallado == "Compañero (a)  permanente" ~ "Compañero(a) permanente",
+            presunto_agresor_detallado == "Compañero (a) permanente" ~ "Compañero(a) permanente",
+            presunto_agresor_detallado == "Compañero (a) de trabajo" ~ "Compañero(a) de trabajo",
+            presunto_agresor_detallado == "Conocido sin ningun trato" ~ "Conocido sin ningún trato",
+            presunto_agresor_detallado == "Cuñado (a)" ~ "Cuñado(a)",
+            presunto_agresor_detallado == "Eln" ~ "ELN",
+            presunto_agresor_detallado == "Empleado (a)" ~ "Empleado(a)",
+            presunto_agresor_detallado == "Esposo" ~ "Esposo(a)",
+            presunto_agresor_detallado == "Esposo (a)" ~ "Esposo(a)",
+            presunto_agresor_detallado == "Ex - Amante" ~ "Ex amante",
+            presunto_agresor_detallado == "Ex - compañero (a) permanente" ~ "Ex compañero(a) permanente",
+            presunto_agresor_detallado == "Ex compañero (a) permanente" ~ "Ex compañero(a) permanente",
+            presunto_agresor_detallado == "Ex - esposo (a)" ~ "Ex esposo(a)",
+            presunto_agresor_detallado == "Ex esposo (a)" ~ "Ex esposo(a)",
+            presunto_agresor_detallado == "Ex - Novio (a)" ~ "Ex novio(a)",
+            presunto_agresor_detallado == "Ex novio (a)" ~ "Ex novio(a)",
+            presunto_agresor_detallado == "Hermano (a)" ~ "Hermano(a)",
+            presunto_agresor_detallado == "Hijo (a)" ~ "Hijo(a)",
+            presunto_agresor_detallado == "Miembro de seguridad privada" ~ " Miembros de seguridad privada",
+            presunto_agresor_detallado == "Novio (a)" ~ "Novio(a)",
+            presunto_agresor_detallado == "Otras Guerrillas" ~ "Otras guerrillas",
+            presunto_agresor_detallado == "Otros familiares civiles y consanguíneos" ~ "Otros familiares civiles o consanguíneos",
+            presunto_agresor_detallado == "Otros familiares civiles o consanguineos" ~ "Otros familiares civiles o consanguíneos",
+            presunto_agresor_detallado == "Paramilitares" ~ "Paramilitares - Autodefensas",
+            presunto_agresor_detallado == "Primo (a)" ~ "Primo(a)",
+            presunto_agresor_detallado == "Profesor (a)" ~ "Profesor(a)",
+            presunto_agresor_detallado == "Suegro (a)" ~ "Suegro(a)",
+            presunto_agresor_detallado == "Tío (a)" ~ "Tío(a)",
+            TRUE ~ presunto_agresor_detallado
+        )
+    )
 
 Homicidio_2015_2024 <- Homicidio_2015_2024 %>%
     mutate(
@@ -85,7 +150,17 @@ Homicidio_2015_2024 <- Homicidio_2015_2024 %>%
             mecanismo_causal_de_la_lesion_fatal == "Agentes y mecanismo explosivo" ~ "Mecanismo o agente explosivo",
             mecanismo_causal_de_la_lesion_fatal == "Otros" ~ "Otro",
             mecanismo_causal_de_la_lesion_fatal == "Otra" ~ "Otro",
-            TRUE ~ mecanismo_causal_de_la_lesion_fatal)
+            TRUE ~ mecanismo_causal_de_la_lesion_fatal),
+        grupo_de_edad_quinquenal = case_when(
+            grupo_de_edad_quinquenal == "Por determinar" ~ "Sin información",
+            TRUE ~ grupo_de_edad_quinquenal
+        ),
+
+        departamento_del_hecho_dane = case_when(
+            departamento_del_hecho_dane == "Bogotá, D.C." ~ "Bogotá D.C.",
+            departamento_del_hecho_dane == "Quindio" ~ "Quindío",
+            TRUE ~ departamento_del_hecho_dane
+        )
     )
 
 
@@ -93,6 +168,10 @@ Homicidio_2015_2024 <- Homicidio_2015_2024 %>%
 Homicidio_2015_2024 <- Homicidio_2015_2024 %>%
     mutate_if(is.character, as.factor) %>% 
     mutate(
+        escenario_del_hecho = case_when(
+            escenario_del_hecho == "parqueaderos,estacionamientos" ~ "parqueaderos, estacionamientos",
+            TRUE ~ escenario_del_hecho
+        ),
         dia_del_hecho = factor(
             dia_del_hecho, 
             levels = c("lunes", "martes", "miércoles", "jueves", "viernes", "sábado", "domingo")
@@ -102,10 +181,33 @@ Homicidio_2015_2024 <- Homicidio_2015_2024 %>%
             mes_del_hecho, 
             levels = c("enero", "febrero", "marzo", "abril", "mayo", "junio", 
                         "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre")
-        )
+        ),
+
+        edad_numerica = case_when(
+            grupo_de_edad_quinquenal == "(00 a 04)" ~ 2.5,
+            grupo_de_edad_quinquenal == "(05 a 09)" ~ 7.5,
+            grupo_de_edad_quinquenal == "(10 a 14)" ~ 12.5,
+            grupo_de_edad_quinquenal == "(15 a 17)" ~ 16.0, 
+            grupo_de_edad_quinquenal == "(18 a 19)" ~ 18.5, 
+            grupo_de_edad_quinquenal == "(20 a 24)" ~ 22.0,
+            grupo_de_edad_quinquenal == "(25 a 29)" ~ 27.0,
+            grupo_de_edad_quinquenal == "(30 a 34)" ~ 32.0,
+            grupo_de_edad_quinquenal == "(35 a 39)" ~ 37.0,
+            grupo_de_edad_quinquenal == "(40 a 44)" ~ 42.0,
+            grupo_de_edad_quinquenal == "(45 a 49)" ~ 47.0,
+            grupo_de_edad_quinquenal == "(50 a 54)" ~ 52.0,
+            grupo_de_edad_quinquenal == "(55 a 59)" ~ 57.0,
+            grupo_de_edad_quinquenal == "(60 a 64)" ~ 62.0,
+            grupo_de_edad_quinquenal == "(65 a 69)" ~ 67.0,
+            grupo_de_edad_quinquenal == "(70 a 74)" ~ 72.0,
+            grupo_de_edad_quinquenal == "(75 a 79)" ~ 77.0,
+            grupo_de_edad_quinquenal == "(80 y más)" ~ 82.5, 
+            grupo_de_edad_quinquenal == "Sin información" ~ NA_real_,
+            grupo_de_edad_quinquenal == "Por determinar" ~ NA_real_,
+            TRUE ~ NA_real_)    
     )
 str(Homicidio_2015_2024)
-
+table(Homicidio_2015_2024$escenario_del_hecho)
 
 # Guardar la base de datos limpia 
 write.csv(Homicidio_2015_2024, "datos/Homicidios_Limpios_2015_2024.csv", row.names = FALSE)
